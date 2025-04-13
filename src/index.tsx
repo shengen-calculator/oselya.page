d48a8bd9be641e17199dbe09f9d6d5cfc579815e
bass <bass@basss-MBP.lan>
2025-04-13 11:15:06 +0300

    introduce checkout page (apply for)

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,12 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import MarketingPage from "./MarketingPage";
 import type {} from '@mui/material/themeCssVarsAugmentation';
+import SignUp from './SignUp';
+import Checkout from "./Checkout";
 
 const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement!);
 
 root.render(
-    <MarketingPage />
+    <Checkout />
 );
