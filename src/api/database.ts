53ebc30c3bcb0c1b38eb0d1537cd1dd325013c62
bass <shengen-calculator@gmail.com>
2025-05-30 19:40:46 +0300

    introduce editorconfig

--- src/api/database.ts
+++ src/api/database.ts
@@ -1,14 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import {getFunctions} from "firebase/functions";
+import { getFunctions } from "firebase/functions";
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID
+    projectId: process.env.REACT_APP_PROJECT_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const functions = getFunctions(app, "europe-west1");
+
